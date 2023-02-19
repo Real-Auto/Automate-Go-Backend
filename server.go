cefd475be619f67120fd91f2df0928a526b10176
Sean <saslynch12@gmail.com>
2023-02-19 18:42:08 +0000

    Added in new details for the swagger

--- server.go
+++ server.go
@@ -1,3 +1,38 @@
+//	Title: AutoMate API
+//	BasePath: /v1
+//	Version: 0.0.1
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+//
+//
+//	SecurityDefinitions:
+//	api_key:
+//	     type: apiKey
+//	     name: KEY
+//	     in: header
+//	oauth2:
+//	    type: oauth2
+//	    authorizationUrl: /oauth2/auth
+//	    tokenUrl: /oauth2/token
+//	    in: header
+//	    scopes:
+//	      bar: foo
+//	    flow: accessCode
+//
+//	Extensions:
+//	x-meta-value: value
+//	x-meta-array:
+//	  - value1
+//	  - value2
+//	x-meta-array-obj:
+//	  - name: obj
+//	    value: field
+//
+// swagger:meta
 package main
 
 import (
